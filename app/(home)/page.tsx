5c6e565258bebd55c766e32f9437c9b79a51f5c1
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2024-03-15 18:08:05 -0300

    feat: add book feed

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,16 +5,19 @@ export default async function Home() {
   const livros = await db.titulos.findMany({
     include: {
       serie: true,
+      autor: true,
     },
   });
 
   return (
     <main>
-      <p>hello world</p>
-      <div className="my-4 flex flex-col gap-2">
-        {livros.map((livro) => (
-          <LivroItem titulo={livro} key={livro.id} />
-        ))}
+      <div className="mx-3 my-4">
+        <h2 className="my-3 text-xl uppercase">Todos os livros</h2>
+        <div className="my-4 flex flex-nowrap gap-2 overflow-x-auto">
+          {livros.map((livro) => (
+            <LivroItem titulo={livro} key={livro.id} />
+          ))}
+        </div>
       </div>
     </main>
   );
--- app/_components/livroItem.tsx
+++ app/_components/livroItem.tsx
@@ -1,23 +1,35 @@
 import { Prisma, titulos } from "@prisma/client";
+import Image from "next/image";
 
 interface LivroItemProps {
   titulo: Prisma.titulosGetPayload<{
     include: {
       serie: true;
+      autor: true;
     };
   }>;
 }
 
 const LivroItem = ({ titulo }: LivroItemProps) => {
   return (
-    <div className="flex flex-col p-3 ">
+    <div className="flex w-[300px] flex-col rounded-sm bg-gray-200 p-3">
+      <Image
+        src={titulo.imageURLs}
+        alt={titulo.titulo}
+        style={{
+          objectFit: "cover",
+        }}
+        width={256}
+        height={300}
+      />
       <div className="flex flex-col p-2">
         {titulo.serie && titulo.numeracaoSerie && (
-          <h2>
+          <h2 className="text-xs text-gray-400">
             {titulo.serie.titulo}: Livro {titulo.numeracaoSerie}
           </h2>
         )}
-        <h2>{titulo.titulo}</h2>
+        <h2 className="text-md">{titulo.titulo}</h2>
+        <p className="text-xs font-light">{titulo.autor.nome}</p>
       </div>
     </div>
   );
